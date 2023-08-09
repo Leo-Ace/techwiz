797f176495d338a0869c581d1db2f18451ecc800
hoangdv0808 <hoangdang8826@gmail.com>
2023-08-10 02:46:42 +0700

    Detail

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/layouts/Main';
 import Home from './components/pages/Home/Home';
+import Detail_member2 from './components/pages/Detail/Detail_member2';
+import Detail_player from './components/pages/Detail/Detail.player';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Main children={<Home />} />} />
+        <Route path='/Detail_player' element={<Main children={<Detail_player />} />} />
+        <Route path='/Detail_member_2' element={<Main children={<Detail_member2/>} />} />
       </Routes>
     </>
   );
