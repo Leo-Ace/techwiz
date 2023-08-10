db21e56a0591f5234836a2da4325fbf8dd0acd92
DinhHuong <huongcbvn204@gmail.com>
2023-08-10 09:33:15 +0700

    a

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Main from './components/layouts/Main';
 import Home from './components/pages/Home/Home';
+import Contact from './components/pages/Contact/Contact';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Main children={<Home />} />} />
+        <Route path='/contact' element={<Main children={<Contact />} />} />
       </Routes>
     </>
   );
