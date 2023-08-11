2014f93b6ab4eac26243d820b12d939be69891c3
Leo-Ace <nguyenthien2282004@gmail.com>
2023-08-12 03:21:02 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Home from './components/pages/Home/Home';
 import Detailplayer from './components/pages/Detail/Detail.player';
 import About from './components/pages/About/About';
 import Squad from './components/pages/squad/Squad';
-import Match from './components/pages/MatchDetail/Match';
-import Detailclub from './components/pages/Detail_club/Detail_club';
 import Club from './components/pages/Detail_club/Detail_club';
 import Contact from './components/pages/Contact/Contact';
+import Login from './components/pages/Login/Login';
+import Register from './components/pages/Register/Register';
+
 function App() {
   return (
     <>
@@ -18,8 +19,10 @@ function App() {
         <Route path='/club/:name/:id' element={<Main children={<Club/>} />} />
         <Route path='/team-table' element={<Main children={<Squad />} />} />
         <Route path='/about' element={<Main children={<About />} />} />
-        <Route path='/club/:name/:id' element={<Main children={<Detailclub/>} />} />
         <Route path='/contact' element={<Main children={<Contact />} />} />
+        <Route path='/login' element={<Main children={<Login/>} />} />
+        <Route path='/register' element={<Main children={<Register/>} />} />
+
       </Routes>
     </>
   );
