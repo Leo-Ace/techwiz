4a9b22aacb851a11f4193d04abff05740e7320d7
hoangdv0808 <hoangdang8826@gmail.com>
2023-08-11 09:17:31 +0700

    u

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Main from './components/layouts/Main';
 import Home from './components/pages/Home/Home';
 import Detail_player from './components/pages/Detail/Detail.player';
 import About from './components/pages/About/About';
-import DetailClub from './components/pages/Detail_club/Detail_club';
 import Squad from './components/pages/squad/Squad';
 import Match from './components/pages/MatchDetail/Match';
 import Detail_club from './components/pages/Detail_club/Detail_club';
@@ -17,7 +16,7 @@ function App() {
         <Route path='/team-table' element={<Main children={<Squad/>} />} />
         <Route path='/home/match' element={<Main children={<Match/>} />} />
         <Route path='/about' element={<Main children={<About />} />} />
-        <Route path='/club/:name/:id' element={<Main children={<DetailClub/>} />} />
+        <Route path='/club/:name/:id' element={<Main children={<Detailclub/>} />} />
       </Routes>
     </>
   );
